e0e171555bc5612deb4f4863aef6324ad00b9d2c
mohamed-aziz <medazizknani@gmail.com>
2019-06-09 20:43:22 +0100

    Add instal deps script

---
+++ installdeps.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#install stuff for elpy
+# Completion and code navigation
+pip install jedi
+# Code checks
+pip install flake8
+# Automatic formatting (PEP8, Yapf or Black)
+pip install autopep8
