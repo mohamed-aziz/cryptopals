6d2a310a1337db5feb2ebd6a1dd0f648c019f03b
mohamed-aziz <medazizknani@gmail.com>
2019-07-24 11:22:43 +0100

    Add ch28

--- set4/__init__.py
+++ set4/__init__.py
@@ -2,3 +2,4 @@
 from .ch25 import edit_ciphertext, break_edit_ciphertext
 from .ch26 import aes_ctr_break_bitflipping, aes_ctr_encryption_oracle
 from .ch27 import encryption_oracle, Not7BitAscii, decryption_oracle, break_cbc_oracle
+from .ch28 import sha1, sha1_mac
--- set4/tests.py
+++ set4/tests.py
@@ -4,9 +4,11 @@ from test import support
 from set1 import aes_ecb_decrypt, unpadpkcs7
 from set2 import generate_random_key
 from set3 import form_nonce, aes_ctr_encrypt_decrypt
-from set4 import edit_ciphertext, break_edit_ciphertext, aes_ctr_break_bitflipping, aes_ctr_encryption_oracle, encryption_oracle, Not7BitAscii, decryption_oracle, break_cbc_oracle
+from set4 import edit_ciphertext, break_edit_ciphertext, aes_ctr_break_bitflipping, aes_ctr_encryption_oracle, encryption_oracle, decryption_oracle, break_cbc_oracle,\
+    sha1, sha1_mac
 from base64 import b64decode
 from functools import partial
+from hashlib import sha1 as orig_sha1
 
 
 class Set4(unittest.TestCase):
@@ -43,6 +45,21 @@ class Set4(unittest.TestCase):
 
         recovered_key = break_cbc_oracle(enc_oracle, dec_oracle)
         self.assertEquals(recovered_key, key)
+
+    def test_ch28(self):
+        # verify implementation is correct
+        hsh = orig_sha1()
+        hsh.update("YELLOW")
+        self.assertEquals(sha1("YELLOW"), hsh.hexdigest())
+
+        # verify can't change message without breaking mac
+        message = "yellow submarine"
+        key = "YELLOW"
+
+        self.assertNotEquals(sha1_mac(key, message), sha1_mac(key, "red submarine"))
+
+    def test_ch29(self):
+        raise NotImplementedError
 
 
 if __name__ == "__main__":
